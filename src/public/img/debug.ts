189eaf0c8f25eba776463963bb400967c201b4ec
BlackFreshMint <freshmint2511@gmail.com>
2025-07-17 02:53:22 -0600

    debug.ts hotfix

--- src/public/img/debug.ts
+++ src/public/img/debug.ts
@@ -3,14 +3,36 @@ import path from 'path';
 import express from 'express';
 
 const app = express();
-const publicPath = path.join(__dirname, 'public');
+const publicPath = path.join(__dirname, 'src');
+
+function listarArchivosRecursivo(dir: string, basePath = dir, nivel = 0): string {
+  let resultado = '';
+  const indent = '  '.repeat(nivel);
+
+  try {
+    const items = fs.readdirSync(dir, { withFileTypes: true });
+    for (const item of items) {
+      const fullPath = path.join(dir, item.name);
+      const relativePath = path.relative(basePath, fullPath);
+      resultado += `${indent}${item.name}${item.isDirectory() ? '/' : ''}\n`;
+      if (item.isDirectory()) {
+        resultado += listarArchivosRecursivo(fullPath, basePath, nivel + 1);
+      }
+    }
+  } catch (err) {
+    resultado += `${indent}Error leyendo ${dir}: ${err}\n`;
+  }
+
+  return resultado;
+}
 
 app.get('/debug-files', (req, res) => {
-  fs.readdir(publicPath, (err, files) => {
-    if (err) {
-      res.status(500).send('Error leyendo archivos');
-    } else {
-      res.send(`<pre>${files.join('\n')}</pre>`);
-    }
-  });
+  const listado = listarArchivosRecursivo(publicPath);
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.send(listado);
 });
+
+// Aquí deberías añadir app.listen si este es todo el archivo
+// Por ejemplo:
+// const PORT = 3000;
+// app.listen(PORT, () => console.log(`Servidor escuchando en puerto ${PORT}`));
